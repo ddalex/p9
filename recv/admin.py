688454a8247aa9850a56582d7595961023c04bee
filetrpastro <filetrpastro@outlook.com>
2013-12-24 12:38:40 +0000

    Recv/UF application commited

---
+++ recv/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from recv.models import UF
+
+admin.site.register(UF)
--- recv/models.py
+++ recv/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UF(models.Model):
+	content = models.TextField()
+	created = models.DateTimeField()
+	remoteIP = models.GenericIPAddressField()
+	useragent = models.CharField(max_length=32)
